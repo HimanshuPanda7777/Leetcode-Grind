50d96b27b257da0414986235d55b36396f7fd3ed
Himanshu Panda <121447495+HimanshuPanda7777@users.noreply.github.com>
2026-03-12 01:13:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.23%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+
+        int mask = 0;
+        int temp = n;
+
+        // create mask with all bits = 1
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+
+        return mask ^ n;
+    }
+}
